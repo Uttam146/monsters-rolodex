3436bd426fd67ec70c7507f921621bb1b110f215
uttam rawat <uttamrawat146@gmail.com>
2020-07-18 13:39:05 +0530

    Updates

---
+++ precache-manifest.a60868b6fb0286e8150f319cf0486a86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964d8bffb4f15586ffc42e35b0c0e3ad",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "83ab63224a69b5bd6c37",
+    "url": "/monsters-rolodex/static/css/main.4710fd83.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ab63224a69b5bd6c37",
+    "url": "/monsters-rolodex/static/js/main.77432493.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
